f2cd9f07fe4ce1986dca0c056093a15241594c76
Adriram04 <adriramgil@gmail.com>
2024-05-07 02:23:17 +0200

    Trabajo con una api real 100% en funcionamiento

--- public/script.js
+++ public/script.js
@@ -1,13 +1,18 @@
-document.getElementById('generateBtn').addEventListener('click', function() {
-    var code = generateCode();
-    document.getElementById('code').innerText = code;
-});
-
-function generateCode() {
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var code = '';
-    for (var i = 0; i < 8; i++) {
-        code += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return code;
-}
+document.getElementById('generateButton').addEventListener('click', function() {
+    // Realiza una solicitud al servidor local en el puerto 3000
+    fetch('http://localhost:3000/generate-code')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al generar el código.');
+        }
+        return response.text();
+      })
+      .then(data => {
+        // Muestra el código generado en la página
+        document.getElementById('code').textContent = data;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
+  
--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const axios = require('axios'); // Importa el módulo axios
 const app = express();
+const port = 3000;
 
+// Middleware para analizar el cuerpo de las solicitudes
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+// Ruta para generar y enviar el código
+app.get('/generate-code', (req, res) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let code = '';
+  for (let i = 0; i < 8; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  // Objeto con el código a enviar
+  const requestBody = { id: code };
+
+  // Simplemente estamos imprimiendo en la consola para demostrar el envío del código
+  console.log('Enviando código:', code);
+
+  // Realiza una solicitud POST a la API en el puerto 8080 utilizando axios
+  axios.post('http://localhost:8080/videoMiner/v1/token', requestBody)
+    .then(response => {
+      console.log('Respuesta de la API:', response.data);
+      res.send('Código generado y enviado con éxito.'+code);
+    })
+    .catch(error => {
+      console.error('Error al enviar código a la API:', error);
+      res.status(500).send('Error al enviar código a la API.');
+    });
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
